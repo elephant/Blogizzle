c1c7ed7797739d371a4dae871b5d35aaeddd0858
Jonathan Suchland <jonathansuchland@gmail.com>
2010-10-28 15:12:46 -0500

    now properly opening and closing mongo connections

--- PostDao.py
+++ PostDao.py
@@ -9,28 +9,29 @@ class PostDao:
     """Retrieve posts"""
 
     def __init__(self):
-        pass
-
-    def connect(self):
-        self.connection = pymongo.Connection()
+        self.connection = pymongo.Connection() 
         self.db = self.connection.blogizzle
+    
+    def __del__(self):
+        self.connection.disconnect()
 
     def save(self, post):
-        self.connect()
         self.db.posts.save(post.__dict__)
 
     def findOne(self):
-        self.connect()
         post = Post(self.db.posts.find_one())
         return post
 
     def find(self, limit = 5, page = 1):
-        self.connect()
         mongoPosts = self.db.posts.find(limit = limit, skip = (limit * (page - 1)))
         posts = PostCollection()
         posts.length = mongoPosts.count()
         posts.currentPage = page
-        posts.totalPages = (posts.length / limit) + (posts.length % limit)
+        if posts.length > limit:
+            posts.totalPages = (posts.length / limit) + (posts.length % limit)
+        else:
+            posts.totalPages = 1
+
         for mongoPost in mongoPosts:
             post = Post(mongoPost)
             posts.add(post)
